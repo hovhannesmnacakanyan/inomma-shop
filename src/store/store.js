e6cee29773fae019b83e00dd3aec8ce457dd6de2
hovomnacakantan <hovomnacakanyan91@gmail.com>
2022-01-23 20:44:33 +0400

    Updates

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import productsReducer from 'reducers/productsSlicer';
-
-export default configureStore({
-  reducer: { products: productsReducer },
-  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
-});
